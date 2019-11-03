4e3560ce6128a9a65facfcb51f7881771241e017
Boren <boren.wang@icloud.com>
2019-11-02 20:32:13 -0400

    Enable Authentication using Passport.js

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     Place = require("./models/place"),
     Comment = require("./models/comment"),
+    User = require("./models/user"),
     seedDB = require("./seeds");
 
 seedDB();
@@ -12,6 +15,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"));
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "CSE",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res){
     res.render("landing")
@@ -28,7 +47,7 @@ app.get("/places", function(req, res){
     })
 });
 
-app.post("/places", function(req, res){
+app.post("/places", isLoggedIn, function(req, res){
     // get data from the form
     var name = req.body.name
     var image = req.body.image 
@@ -45,7 +64,7 @@ app.post("/places", function(req, res){
     });
 });
 
-app.get("/places/new", function(req, res){
+app.get("/places/new", isLoggedIn, function(req, res){
     res.render("places/new");
 });
 
@@ -63,7 +82,7 @@ app.get("/places/:id", function(req, res){
 });
 
 
-app.get("/places/:id/comments/new", function(req, res){
+app.get("/places/:id/comments/new", isLoggedIn, function(req, res){
     //find campground by id
     Place.findById(req.params.id, function(err, place){
         if(err){
@@ -74,7 +93,7 @@ app.get("/places/:id/comments/new", function(req, res){
      });
 });
 
-app.post("/places/:id/comments", function(req, res){
+app.post("/places/:id/comments", isLoggedIn, function(req, res){
     // look up campground using ID
     Place.findById(req.params.id, function(err, place){
         if(err){
@@ -94,6 +113,48 @@ app.post("/places/:id/comments", function(req, res){
     });
 });
 
-app.listen(5000, process.env.IP, function(){
+// AUTH ROUTE
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    // res.send("Signing you up!");
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/places");
+        });
+    });
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/places",
+    failureRedirect: "/login"
+}) ,function(req, res){
+
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/places");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } 
+    res.redirect("/login");
+}
+
+app.listen(3000, process.env.IP, function(){
     console.log("YelpLongIsland Has Started!")
 });
